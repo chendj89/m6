831337550430d4ddd92d422f783f177b9f8bd39e
daijianchen <719857499@qq.com>
2023-05-12 17:59:01 +0800

    定义用户数据

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,3 +1,4 @@
+import type { BadgeProps } from 'naive-ui'
 /**
  * 用户信息
  */
@@ -6,4 +7,25 @@ export interface userInfo {
   label?: string
   type?: string
   render?: () => any
+}
+
+export type IMenu = Array<'menu' | 'tools' | 'help'>
+export interface IUserMsg {
+  menu: IMenu
+}
+
+export type Features = Record<string, any>
+
+export interface IUserDataItem {
+  name: string
+  desc: string
+  url: string
+  icon: string
+  badge?: BadgeProps,
+  features?:Features
+}
+
+export interface IUserData extends IUserDataItem {
+  info?: IUserMsg
+  children?:IUserDataItem[]
 }
